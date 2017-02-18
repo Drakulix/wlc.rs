ed63374f9b01a344600f95fa4e4e38e27c916d81
Victor Brekenfeld (Drakulix) <github@drakulix.de>
2017-02-18 22:20:29 +0100

    Update dependencies

--- wlc-sys/src/lib.rs
+++ wlc-sys/src/lib.rs
@@ -6,6 +6,6 @@ use libc::c_void;
 type wl_resource = c_void;
 type wl_client = c_void;
 type wl_display = c_void;
-type wl_message_wl_interface = c_void;
+type wl_interface = c_void;
 
 include!("gen.rs");
